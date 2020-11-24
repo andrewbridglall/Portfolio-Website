bff4492bbbabbba5aef33a9bbdf665cde863da30
Andrew Bridglall <66570568+andrewbridglall@users.noreply.github.com>
2020-11-24 17:43:27 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">    
+      <p>
+      <a className="App-link" href="https://drive.google.com/file/d/1IefhYWz2ArlSLNSPB2boZVLlR-R9seT2/view?usp=sharing" target="_blank" rel="noopener noreferrer">Resume</a>&nbsp;|&nbsp;
+      <a className="App-link" href="https://github.com/andrewbridglall" target="_blank" rel="noopener noreferrer">Github</a>&nbsp;|&nbsp;
+      <a className="App-link" href="https://www.linkedin.com/in/andrewbridglall/" target="_blank" rel="noopener noreferrer">LinkedIn</a>&nbsp;|&nbsp;
+      <a className="App-link" href="mailto:akb101@georgetown.edu" target="_blank" rel="noopener noreferrer">Email</a>
+        </p>    
+      Hi, I'm Andrew Bridglall.
+        <p>
+          Math+CS at Georgetown. Class of 2021. Ex-WeWork Labs.
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;

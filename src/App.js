17cd25910ac8673c5ce767e59a0d5b4b61a2db36
Andrew Bridglall <66570568+andrewbridglall@users.noreply.github.com>
2021-02-07 15:09:47 -0500

    update resume

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">    
       <p>
-      <a className="App-link" href="https://drive.google.com/file/d/1w1By7IwPrYxjmLFnWdwlNkUjAIBCXRhc/view?usp=sharing" target="_blank" rel="noopener noreferrer">Resume</a>&nbsp;|&nbsp;
+      <a className="App-link" href="https://drive.google.com/file/d/1ORIsB_SjvL4MP640ru5FhrI4BxWTTYUX/view?usp=sharing" target="_blank" rel="noopener noreferrer">Resume</a>&nbsp;|&nbsp;
       <a className="App-link" href="https://github.com/andrewbridglall" target="_blank" rel="noopener noreferrer">Github</a>&nbsp;|&nbsp;
       <a className="App-link" href="https://www.linkedin.com/in/andrewbridglall/" target="_blank" rel="noopener noreferrer">Linkedin</a>&nbsp;|&nbsp;
       <a className="App-link" href="mailto:andrew.bridglall@gmail.com" target="_blank" rel="noopener noreferrer">Email</a>
